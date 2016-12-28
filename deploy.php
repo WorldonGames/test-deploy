045e237ced76d3b78b10340c3075ceff0dc3c90a
Fabio G <gughy14@worldongames.it>
2016-12-29 00:11:55 +0100

    Wrong branch

--- deploy.php
+++ deploy.php
@@ -1,20 +1,24 @@
 <?php
 	
-	$secret = 'ChiaveErrata';
+	$secret = 'Culo!';
 	$sign = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 	
 	list($sha, $hash) = explode('=', $sign, 2);
 	
-	$payload = file_get_contents('php://input');
 	
-	// Calculate hash based on payload and the secret
-	$payloadHash = hash_hmac($sha, $payload, $secret);
+	$json = file_get_contents('php://input');
+	$jsonHash = hash_hmac($sha, $json, $secret);
 	
-	// Check if hashes are equivalent
-	if($hash !== $payloadHash){
+	$payload = json_decode($json);	
+	
+	if($hash !== $jsonHash){
 		die('Foca!!!');
+	}
+	
+	if($payload->ref == "refs/heads/culocane"){
+		echo("branch OK");
 	}else{
-		echo('WORKZZZ');
+		echo("merda");
 	}
 
 ?>
