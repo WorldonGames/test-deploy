725bb3d7e6baf7f6d9337bb8ba6331fc785b13df
Fabio G <gughy14@worldongames.it>
2016-12-29 00:08:33 +0100

    Maybe...

--- deploy.php
+++ deploy.php
@@ -5,12 +5,12 @@
 	
 	list($sha, $hash) = explode('=', $sign, 2);
 	
-	$payload = file_get_contents('php://input');
 	
-	// Calculate hash based on payload and the secret
+	$json = file_get_contents('php://input');
+	$payload = json_decode($json);	
+	
 	$payloadHash = hash_hmac($sha, $payload, $secret);
 	
-	// Check if hashes are equivalent
 	if($hash !== $payloadHash){
 		die('Foca!!!');
 	}
