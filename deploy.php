025d5538cbea41795b28d88216763540a68a3e8b
Fabio G <gughy14@worldongames.it>
2016-12-28 21:08:54 +0100

    Maybe something serious

--- deploy.php
+++ deploy.php
@@ -1,8 +1,20 @@
 <?php
+	
+	$secret = 'Culo!';
+	$sign = $_SERVER['HTTP_X-Hub-Signature'];
+	
+	list($sha, $hash) = explode('=', $sign, 2);
+	
+	$payload = file_get_contents('php://input');
+	
+	// Calculate hash based on payload and the secret
+	$payloadHash = hash_hmac($algo, $payload, $secret);
+	
+	// Check if hashes are equivalent
+	if($hash !== $payloadHash){
+		die('Foca!!!');
+	}else{
+		echo('WORKZZZ');
+	}
 
-if(isset($_GET['sat']) && ($_GET['sat'] == "Culo!")){
-	echo("Cazzo");
-}else{
-	echo("Shit");
-}
 ?>
