d8ca1f273cf2ff9632dedd5003438850312892f2
core-kd93 <supporto@worldongames.net>
2016-12-29 20:27:02 +0000

    asdasdas

--- culo.bat
+++ culo.bat
@@ -1,5 +1,2 @@
-cd C:\inetpub\wwwroot\test-deploy 
 git fetch
 git merge
-
-pause
--- culo.php
+++ culo.php
@@ -1,10 +1,4 @@
 <?php
-
-
-
-
 $output = shell_exec('C:\inetpub\wwwroot\test-deploy\culo.bat');
 echo "<pre>$output</pre>";
-
-
 ?>
