181d82e3cb03511924598c93037c812fa319fcdb
Fabio G <gughy14@worldongames.it>
2016-12-29 19:34:34 +0100

    Def

---
+++ deploy.php
@@ -0,0 +1,30 @@
+<?php
+	
+	$secret = 'Culo!';
+	$sign = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+	
+	list($sha, $hash) = explode('=', $sign, 2);
+	
+	
+	$json = file_get_contents('php://input');
+	$jsonHash = hash_hmac($sha, $json, $secret);
+	
+	$payload = json_decode($json);	
+	
+	if($hash !== $jsonHash){
+		die('Foca!!!');
+	}
+	
+	if($payload->ref == "refs/heads/culocane"){
+		echo("branch OK");
+
+$out = array();
+shell_exec('cd C:\inetpub\wwwroot\test-deploy && git merge');
+
+
+
+	}else{
+		echo("merda");
+	}
+
+?>
